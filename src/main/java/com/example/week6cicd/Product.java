909107c73d544a728e4a9449104cecb02179117e
Nasir Ahmad <G00419630@atu.ie>
2024-10-23 15:09:04 +0100

    added Validation

--- src/main/java/com/example/week6cicd/Product.java
+++ src/main/java/com/example/week6cicd/Product.java
@@ -1,5 +1,9 @@
 package com.example.week6cicd;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +12,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
+    @Min(value=0,message="id value must be greater than 1")
     private int id;
+    @NotBlank(message="Product name cannot be blank")
     private String name;
+    @PositiveOrZero(message = "Price cannot be negative")
     private double price;
 }
--- src/main/java/com/example/week6cicd/ProductController.java
+++ src/main/java/com/example/week6cicd/ProductController.java
@@ -1,5 +1,6 @@
 package com.example.week6cicd;
 
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import java.util.List;
 public class ProductController {
 
     private ProductService myProduct;
+    private Product product;
 
     public ProductController(ProductService myProduct) {
         this.myProduct = myProduct;
@@ -22,8 +24,9 @@ public class ProductController {
 
 
     @PostMapping("/newProduct")
-    public List<Product> newProduct(@RequestBody Product product)
+    public List<Product> newProduct(@Valid @RequestBody Product product)
     {
+        this.product = product;
         //business logic to add this to a database
         //return list of all products from the database
         return myProduct.addProduct(product);
